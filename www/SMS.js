6cc027937e80d7434c2e599b64a735732ccc2e7b
vito <vitohe@users.noreply.github.com>
2017-08-11 11:51:51 +0800

    Update SMS.js

--- www/SMS.js
+++ www/SMS.js
@@ -41,39 +41,10 @@ safesmsExport.enableIntercept = function(on_off, successCallback, failureCallbac
 	cordova.exec( successCallback, failureCallback, 'SMS', 'enableIntercept', [ on_off ] );
 };
 
-safesmsExport.sendSMS = function(address, text, successCallback, failureCallback) {
-	var numbers;
-	if( Object.prototype.toString.call( address ) === '[object Array]' ) {
-		numbers = address;
-	} else if(typeof address === 'string') {
-		numbers = [ address ];
-	} else {
-		if(typeof failureCallback === 'function') {
-			failureCallback("require address, phone number as string, or array of string");
-		}
-		return;
-	}
-	
-	cordova.exec( successCallback, failureCallback, 'SMS', 'sendSMS', [ numbers, text ] );
-};
-
 safesmsExport.listSMS = function(filter, successCallback, failureCallback) {
 	cordova.exec( successCallback, failureCallback, 'SMS', 'listSMS', [ filter ] );
 };
 
-safesmsExport.deleteSMS = function(filter, successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'SMS', 'deleteSMS', [ filter ] );
-};
-
-safesmsExport.restoreSMS = function(msg, successCallback, failureCallback) {
-	var smsList = [];
-	if(Array.isArray(msg)) {
-		if(msg.length > 0) smsList = msg;
-	} else if(typeof msg === 'object') {
-		if(msg !== null) smsList = [ msg ];
-	}
-	cordova.exec( successCallback, failureCallback, 'SMS', 'restoreSMS', [ msg ] );
-};
 
 /*
  * Events:
